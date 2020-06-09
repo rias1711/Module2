8cc979ac584eb5cabfc0647d8a1024fc82c78941
rias1711 <64952470+rias1711@users.noreply.github.com>
2020-06-09 08:50:00 +0700

    update

--- src/Day1/Forex.java
+++ src/Day1/Forex.java
@@ -5,12 +5,32 @@ import java.util.Scanner;
 public class Forex {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double dollar;
+        int choose;
 
-        System.out.print("Nhập vào Dollar: ");
-        dollar = scanner.nextInt();
+        System.out.print("Nhập 1 để đổi Dollar - VND, 2 để đổi VND - Dollar: ");
+        choose = scanner.nextInt();
 
-        double rate = dollar * 23000;
-        System.out.print("VND: " + rate);
+        switch (choose) {
+            case 1:
+                Scanner dv = new Scanner(System.in);
+                double dollar;
+                System.out.print("Nhập tiền Dollar: ");
+                dollar = dv.nextDouble();
+                double rate = dollar * 23000;
+                System.out.print("VND: " + rate);
+                break;
+            case 2:
+                Scanner vd = new Scanner(System.in);
+                double vnd;
+                System.out.print("Nhập tiền VND: ");
+                vnd = vd.nextDouble();
+                double rate2 = vnd / 23000;
+                System.out.print("Dollar: " + rate2);
+                break;
+            default:
+                System.out.print("Lỗi!");
+        }
+
+
     }
 }
