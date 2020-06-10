8d1fbc0be7eca926c43942283a472c3ab6425abd
rias1711 <64952470+rias1711@users.noreply.github.com>
2020-06-10 08:55:53 +0700

    day 3

---
+++ src/Week1/Day3/ChangeDegree.java
@@ -0,0 +1,46 @@
+package Week1.Day3;
+
+import java.util.Scanner;
+
+public class ChangeDegree {
+    public static void main(String[] args) {
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter a number you choose: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter your Fahrenheit: ");
+                    fahrenheit = scanner.nextDouble();
+                    System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));
+                    break;
+                case 2:
+                    System.out.println("Enter your Celsius: ");
+                    celsius = scanner.nextDouble();
+                    System.out.println("Celsius to Fahrenheit: " + celsiusToFahrenheit(celsius));
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (choice != 0);
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        return fahrenheit;
+    }
+}
