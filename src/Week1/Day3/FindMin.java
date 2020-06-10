802eb80b62ebbbf19415356f115b5f844cad5df2
rias1711 <64952470+rias1711@users.noreply.github.com>
2020-06-10 09:03:46 +0700

    day 3

---
+++ src/Week1/Day3/FindMin.java
@@ -0,0 +1,18 @@
+package Week1.Day3;
+
+public class FindMin {
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + index);
+    }
+    public static int minValue(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
